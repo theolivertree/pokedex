fae2e0b2d18962caa997bdcc7becc47091fdafeb
theolivertree <gdo143@gmail.com>
2023-04-17 12:49:27 -0300

    Poke Info UI commit

--- src/components/PokeID/PokeID.jsx
+++ src/components/PokeID/PokeID.jsx
@@ -5,8 +5,11 @@ export default function PokeID() {
 
     return (
         <div className='PokeID'>
-            <p>ID: 001</p>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" className='PokePhoto' />
+            <div className='PokeIdImageName'>
+                <p>ID: 001</p>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" className='PokePhoto' />
+                <p>Bulbasaur</p>
+            </div>
             <div className='TypeandStatsDiv'>
 
             </div>
